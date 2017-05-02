a8fbffbed914759cf5213d851b15a05eebaaa925
jacquesRud <jacques.rudloff@gmail.com>
2017-05-02 15:11:44 +0200

    Intefrace

---
+++ fr/n7/stl/block/ast/impl/InterfaceImpl.java
@@ -0,0 +1,22 @@
+package fr.n7.stl.block.ast.impl;
+
+import fr.n7.stl.block.ast.ElementInterface;
+import fr.n7.stl.block.ast.Expression;
+import fr.n7.stl.block.ast.Interface;
+import fr.n7.stl.block.ast.Type;
+import fr.n7.stl.tam.ast.Fragment;
+import fr.n7.stl.tam.ast.TAMFactory;
+
+public class InterfaceImpl implements Interface {
+
+	protected String _name;
+	protected Expression _heritageInteface;
+	protected ElementInterface _methodes;
+
+	public InterfaceImpl(String _name, Expression _heritageInteface, ElementInterface _methodes) {
+		this._name=_name;
+		this._heritageInteface=_heritageInteface;
+		this._methodes=_methodes;
+	}
+
+}
