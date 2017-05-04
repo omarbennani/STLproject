0659ad3971f15c7eb906559495551976e90a6cf0
Soul31 <louis.chausserielapree@gmail.com>
2017-05-04 16:29:11 +0200

    Creation de la classe StringValue

---
+++ fr/n7/stl/block/ast/impl/StringValueImpl.java
@@ -0,0 +1,32 @@
+package fr.n7.stl.block.ast.impl;
+
+import fr.n7.stl.block.ast.AtomicType;
+import fr.n7.stl.block.ast.StringValue;
+import fr.n7.stl.block.ast.Type;
+import fr.n7.stl.tam.ast.Fragment;
+import fr.n7.stl.tam.ast.TAMFactory;
+
+public class StringValueImpl implements StringValue{
+	
+	private String value;
+	
+	public StringValueImpl(String _text) {
+		this.value = _text;
+	}
+
+	public String toString() {
+		return this.value;
+	}
+	
+	@Override
+	public Type getType() {
+		return AtomicType.StringType;
+	}
+
+	@Override
+	public Fragment getCode(TAMFactory _factory) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
