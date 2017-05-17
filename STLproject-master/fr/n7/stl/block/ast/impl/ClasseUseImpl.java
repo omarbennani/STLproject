3df45406c0e3d5f1592db313c1cd672a36d59cb9
yaoBohoussou <bohoussou1995@gmail.com>
2017-05-17 18:38:10 +0200

    ClasseUse et InterfaceUse

---
+++ STLproject-master/fr/n7/stl/block/ast/impl/ClasseUseImpl.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package fr.n7.stl.block.ast.impl;
+
+import fr.n7.stl.block.ast.Expression;
+import fr.n7.stl.block.ast.Classe;
+import fr.n7.stl.block.ast.Type;
+import fr.n7.stl.block.ast.VariableDeclaration;
+import fr.n7.stl.tam.ast.Fragment;
+import fr.n7.stl.tam.ast.TAMFactory;
+
+/**
+ * Implementation of the Abstract Syntax Tree node for a variable use expression.
+ * @author Marc Pantel
+ *
+ */
+public class ClasseUseImpl implements Expression {
+
+	protected Classe classe;
+	
+	/**
+	 * Creates a variable use expression Abstract Syntax Tree node.
+	 * @param _declaration Declaration of the used variable.
+	 */
+	public ClasseUseImpl(Classe _classe) {
+		this.classe = _classe;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+			return ("@{" + this.classe.getName() + "}");
+	}
+
+	/* (non-Javadoc)
+	 * @see fr.n7.stl.block.ast.Expression#getType()
+	 */
+	@Override
+	public Type getType() {
+		return classe.getType();
+	}
+
+	/* (non-Javadoc)
+	 * @see fr.n7.stl.block.ast.Expression#getCode(fr.n7.stl.tam.ast.TAMFactory)
+	 */
+	@Override
+	public Fragment getCode(TAMFactory _factory) {
+		throw new SemanticsUndefinedException("getCode is not defined in ClasseUseImpl");
+	}
+
+}
