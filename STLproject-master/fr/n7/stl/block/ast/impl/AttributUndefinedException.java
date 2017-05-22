68d7f39a3465300811a8b3a72ab66504432ab9f8
Soul31 <louis.chausserielapree@gmail.com>
2017-05-22 11:37:54 +0200

    Ajout de l'exception attribut non défini

---
+++ STLproject-master/fr/n7/stl/block/ast/impl/AttributUndefinedException.java
@@ -0,0 +1,8 @@
+package fr.n7.stl.block.ast.impl;
+
+public class AttributUndefinedException extends RuntimeException {
+
+	public AttributUndefinedException(String message) {
+		super(message);
+	}
+}
